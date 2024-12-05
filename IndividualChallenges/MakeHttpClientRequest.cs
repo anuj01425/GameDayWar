6e331cf70319e43ff5676a4ce24343d8cd306d89
Nikhil Garg <nikhilgarg@microsoft.com>
2024-12-05 16:07:08 +0530

    Changing from http to https

--- IndividualChallenges/MakeHttpClientRequest.cs
+++ IndividualChallenges/MakeHttpClientRequest.cs
@@ -2,7 +2,7 @@ public void MakeHttpClientRequest(string host)
 {
     var client = new HttpClient
     {
-        BaseAddress = new Uri("http://" + host) // Insecure: Uses HTTP instead of HTTPS
+        BaseAddress = new Uri("https://" + host) // Insecure: Uses HTTP instead of HTTPS
     };
     var response = client.GetAsync("/api/data").Result;
     Console.WriteLine("Response status: " + response.StatusCode);
