90b01ea531e7196058417b61a69fd297d31aaa7a
anuj01425 <anuj01425@gmail.com>
2024-12-05 16:15:13 +0530

    Update InsecureBase64Encryption.cs

--- IndividualChallenges/InsecureBase64Encryption.cs
+++ IndividualChallenges/InsecureBase64Encryption.cs
@@ -1,5 +1,25 @@
 public void InsecureBase64Encryption(string data)
 {
-    var encryptedData = Convert.ToBase64String(Encoding.UTF8.GetBytes(data)); // Not encryption
-    Console.WriteLine($"Base64 Encrypted Data: {encryptedData}");
+   byte[] key = Encoding.UTF8.GetBytes("A5D1F6E7B3C8A9F1"); 
+   byte[] iv = Encoding.UTF8.GetBytes("1234567890ABCDEF");
+
+   using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.IV = iv;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(data);
+                    }
+                }
+                var encryptedData = Convert.ToBase64String(ms.ToArray());
+                Console.WriteLine($"AES Encrypted Data: {encryptedData}");
+            }
+    }
 }
